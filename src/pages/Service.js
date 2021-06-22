003b0ef1bb21db23062487502d4e3f1c90f363ce
Luis Ascencio <luismiguelascencio@gmail.com>
2021-06-22 10:35:32 -0400

    favicon

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,39 +1,39 @@
-import React, { Fragment } from "react"; 
+import React, { Fragment } from "react";
 import MetaTags from "react-meta-tags";
-import LayoutOne from "../layouts/LayoutOne"; 
+import LayoutOne from "../layouts/LayoutOne";
 import PageHeader from '../components/PageHeader'
-import Services from '../components/Services/home-one' 
+import Services from '../components/Services/home-one'
 import Funfact from '../components/FunFact/home-one'
-import Testimonial from '../components/Testimonials/home-one'; 
-import LogoContainer from '../components/BrandLogo';  
+import Testimonial from '../components/Testimonials/home-one';
+import LogoContainer from '../components/BrandLogo';
 
 const servicePage = () => {
 	return (
-	  <Fragment>
-		<MetaTags>
-		  <title>The Surrogacy Law Firm | Home</title>
-		  <meta
-			name="description"
-			content="Multipage Business  React JS Template."
-		  />
-		</MetaTags>
-		<LayoutOne>
-		<div className="service-page">
-			<PageHeader
-				bgImg={require('../assets/images/page_bg.jpg')}
-				title="Our Services" 
-			/>
-			<Services /> 
-			<Funfact classes="funfact_serv" /> 
-			<Testimonial /> 
-			<LogoContainer /> 
-		</div>
-		</LayoutOne>
-	  </Fragment>
+		<Fragment>
+			<MetaTags>
+				<title>The Surrogacy Law Firm | What We Do</title>
+				<meta
+					name="description"
+					content="The Surrogacy Law Firm What We Do page."
+				/>
+			</MetaTags>
+			<LayoutOne>
+				<div className="service-page">
+					<PageHeader
+						bgImg={require('../assets/images/page_bg.jpg')}
+						title="Our Services"
+					/>
+					<Services />
+					{/* <Funfact classes="funfact_serv" />
+					<Testimonial />
+					<LogoContainer /> */}
+				</div>
+			</LayoutOne>
+		</Fragment>
 	);
-  };
-  
-  export default servicePage;
+};
+
+export default servicePage;
 
 
 
